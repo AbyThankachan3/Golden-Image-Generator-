95c369cc5d66107c2743bc35035e819a06cdcdc0
Aby Thankachan <abythankachan.314@gmail.com>
2026-03-24 16:07:28 +0530

    Fix: add --platform linux/amd64 to GRUB hash generation

--- .github/scripts/generate-grub-hash.sh
+++ .github/scripts/generate-grub-hash.sh
@@ -14,7 +14,7 @@ echo "::add-mask::${GRUB_PASSWORD}"
 echo "Generating GRUB PBKDF2 hash..."
 
 HASH=$(echo -e "${GRUB_PASSWORD}\n${GRUB_PASSWORD}" | \
-    docker run --rm -i ubuntu:22.04 bash -c \
+    docker run --rm -i --platform linux/amd64 ubuntu:22.04 bash -c \
     "apt-get update -qq >/dev/null 2>&1 && \
      apt-get install -y -qq grub-common >/dev/null 2>&1 && \
      grub-mkpasswd-pbkdf2 2>/dev/null" | \
